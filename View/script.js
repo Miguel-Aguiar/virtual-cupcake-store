f2aff679969dd315ba9277414a165fa67b06fc64
Miguel Aguiar <miguelaguiardossantos@gmail.com>
2024-11-01 18:42:11 -0300

    Crie seu Cupcake finalizado

--- View/script.js
+++ View/script.js
@@ -1,6 +1,21 @@
 function adicionarAoCarrinho() {
-    // Mostra a notificação
     const notification = document.getElementById('notification');
+    
+    const currentPath = window.location.pathname;
+    if(currentPath == "/View/criarCupcake.html"){
+        const sabores = document.querySelectorAll('.selected');
+        if(sabores.length != 3){
+
+            notification.innerHTML = "Selecione os sabores!"
+            notification.style.backgroundColor = "#d90000"
+            // return;
+        } else {
+            notification.innerHTML = "Item adicionado ao carrinho!";
+            notification.style.backgroundColor = "#4CAF50"
+        }
+    }
+
+    // Mostra a notificação
     notification.classList.add('show');
 
     // Oculta a notificação após 3 segundos
@@ -19,4 +34,27 @@ function toggleCart() {
 function escolherSabor() {
     const chooseFlavor = document.getElementById('choose-flavor');
     chooseFlavor.classList.toggle('show');
+}
+
+// Seleciona sabor no criar cupcake
+function selecionarSabor(element, tipo) {
+    const saboresMassa = document.querySelectorAll('.sabor.massa');
+    const saboresRecheio = document.querySelectorAll('.sabor.recheio');
+    const saboresCobertura = document.querySelectorAll('.sabor.cobertura');
+    
+    switch(tipo){
+        case 'massa':
+            saboresMassa.forEach(sabor => sabor.classList.remove('selected'));
+            break;
+            
+        case 'recheio':
+            saboresRecheio.forEach(sabor => sabor.classList.remove('selected'));
+            break;
+
+        case 'cobertura':
+            saboresCobertura.forEach(sabor => sabor.classList.remove('selected'));
+            break;
+    }
+
+    element.classList.add('selected');
 }
