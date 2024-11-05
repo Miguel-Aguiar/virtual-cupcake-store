6b07e071520efe0e46018f1f09ecca1c77cc2dcb
Miguel Aguiar <miguelaguiardossantos@gmail.com>
2024-11-05 17:43:39 -0300

    Carrinho finalizado e alguns pequenos ajustes em outras páginas feitos

--- View/script.js
+++ View/script.js
@@ -33,6 +33,7 @@ function toggleCart() {
 // Mostra o escolher sabor da página combos
 function escolherSabor() {
     const chooseFlavor = document.getElementById('choose-flavor');
+    // chooseFlavor.style.display = 'block';
     chooseFlavor.classList.toggle('show');
 }
 
@@ -57,4 +58,69 @@ function selecionarSabor(element, tipo) {
     }
 
     element.classList.add('selected');
+}
+
+// Scroll horizontal 
+
+const carrinhoContainer = document.querySelector('.carrinho-itens');
+let scrollAmount = 0; // Armazena o valor da rolagem a ser aplicado
+let isScrolling = false; // Verifica se já está rolando
+
+carrinhoContainer.addEventListener('wheel', (event) => {
+    event.preventDefault();
+    scrollAmount += event.deltaY * 0.1; // Ajuste a multiplicação para alterar a velocidade (0.3 é o fator de suavização)
+
+    if (!isScrolling) {
+        smoothScroll();
+    }
+});
+
+function smoothScroll() {
+    isScrolling = true;
+    // Reduz gradualmente o valor de scrollAmount para um efeito de suavização
+    scrollAmount *= 0.8;
+    carrinhoContainer.scrollLeft += scrollAmount;
+
+    // Continua rolando enquanto o valor absoluto de scrollAmount for significativo
+    if (Math.abs(scrollAmount) > 0.5) {
+        requestAnimationFrame(smoothScroll);
+    } else {
+        isScrolling = false; // Para a animação quando quase não houver movimento
+    }
+}
+
+function scrollar(lado) {
+    const deslocamento = lado == 'left' ? -240 : 240;
+    carrinhoContainer.scrollBy({
+        left: deslocamento,
+        behavior: 'smooth'
+    });
+}
+
+// Fechar pedido
+function fecharPedido(){
+    const fecharPedido = document.getElementById('FecharPedido');
+    fecharPedido.classList.toggle('show')
+}
+
+// Selecionar método de pagamento
+function selecionarMetodoPagamento(element, tipo) {
+    const metodoPagamento = document.querySelectorAll('div.opcao');
+    metodoPagamento.forEach(opcao => opcao.classList.remove('selected'));
+    element.classList.add('selected')
+    
+    if(tipo == 'credito'){
+        // formulario desativado
+    } else if(tipo == 'pix'){
+        //abrir popup esperar 3 sec dar pagamento confirmado 
+
+    } else {
+        // dinhero, nao precisa fazer nada
+    }
+}
+
+// Realizar pagamento
+function realizarPagamento() {
+    window.location.href = "http://127.0.0.1:5500/View/pedidos.html";
+    
 }
